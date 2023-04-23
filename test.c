6a41fb3391e8045fe5783469e50c0fa604a2a3c5
Jihun Yun <yunjyhoon@cau.ac.kr>
2023-04-23 23:25:53 +0900

    test.c 1차 커밋

---
+++ test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+// {
+//     char ch;
+
+//     int a = 10;
+//     int b = 20;
+
+//     float result = 0;
+
+//     printf("연산자를 입력해주세요");
+//     scanf("%d %d", &a, &b);
+//     scanf(" %c", &ch);
+
+//     switch (ch)
+//     {
+//     case '+':
+//         printf("%d + %d = %d", a, ch, b);
+//         break;
+//     default:
+//         printf("디폴트임");
+//         break;
+//     }
+// }
+
+{
+    char a = 'c';
+    printf("%c\n", 'A' - 'a' + a);
+    printf("%d", 'A' - 'a');
+}
+
+// 커밋관련 테스트 - 스테이징 할거임
---
+++ test.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+
+for i in range(a,b+1):
+    print(i)
+    
